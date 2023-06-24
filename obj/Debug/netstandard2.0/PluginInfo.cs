ac02d8c3fd9b3dbec5624093c5995c2cdc6bed04
Dalton Donovan <90434265+daltonyxdonovan@users.noreply.github.com>
2023-06-24 00:40:02 -0500

    initial commit

---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ColonySurvival_ConsoleCommands
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ColonySurvival_ConsoleCommands";
+        public const string PLUGIN_NAME = "ColonySurvival_ConsoleCommands";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
